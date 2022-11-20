f7e21788b5dc07f93d19e9385a51daf0b8369924
Anju014 <84642983+Anju014@users.noreply.github.com>
2022-11-20 14:39:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
+var ball;
+
 function preload(){
   
 }
 
 function setup() {
   createCanvas(400,400);
+  
+  //ball sprite
+  ball = createSprite(200,200,30,30);
+  ball.shapeColor = "#FB2576";
 
 }
 
 function draw() {
-  background("black");  
+  background("black"); 
+  
+  //change background color to "#DEBACE" when up arrow is pressed
+  
+  if(keyDown("up"){
+     background("#DEBACE");
+  }
+
+  //change background color to "#CFF5E7" when down arrow is pressed
+
+
+  //change background color to "#B9E0FF" when left arrow is pressed
+
+
+
+  //change background color to "#FFE1E1" when right arrow is pressed
+
+
   drawSprites();
 }
